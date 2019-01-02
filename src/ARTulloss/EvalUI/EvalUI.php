4f6124e5e110464d33dd7670ee841b48b50f0cdb
artulloss <35859316+artulloss@users.noreply.github.com>
2019-01-02 16:11:16 -0500

    eval for commands fix
    S t o l e n

--- src/ARTulloss/EvalUI/EvalUI.php
+++ src/ARTulloss/EvalUI/EvalUI.php
@@ -17,48 +17,44 @@ use pocketmine\utils\TextFormat;
  */
 class EvalUI extends PluginBase implements Listener
 {
-	public function onEnable(): void
-	{
-		$this->getServer()->getCommandMap()->register('EvalUI', new EvalCommand('eval', 'Eval as a UI!'));
-	}
+    public function onEnable(): void
+    {
+        $this->getServer()->getCommandMap()->register('EvalUI', new EvalCommand('eval', 'Eval as a UI!'));
+    }
 }
 
 class EvalCommand extends Command
 {
-	public function execute(CommandSender $sender, string $commandLabel, array $args)
-	{
-		$server = $sender->getServer();
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    {
+        $server = $sender->getServer();
 
-		if($sender instanceof Player) {
+        if($sender instanceof Player) {
 
-			$closure = function (Player $sender, $data) use ($server): void
-			{
-				if(isset($data)) {
-					try {
-						eval($data[0]);
-					} catch (\ParseError $exception) {
-						$sender->sendMessage(TextFormat::RED . 'You made an oopsie but we caught it! . Error: ' . $exception->getMessage());
-					}
-				}
-			};
+            $closure = function (Player $sender, $data) use ($server): void
+            {
+                if(isset($data)) {
+                    try {
+                        eval($data[0]);
+                    } catch (\ParseError $exception) {
+                        $sender->sendMessage(TextFormat::RED . 'You made an oopsie but we caught it! . Error: ' . $exception->getMessage());
+                    }
+                }
+            };
 
-			$form = new CustomForm($closure);
+            $form = new CustomForm($closure);
 
-			$form->addInput('What do you want to eval?', '$sender = You, $server = Server, Enjoy!');
+            $form->addInput('What do you want to eval?', '$sender = You, $server = Server, Enjoy!');
 
-			$sender->sendForm($form);
+            $sender->sendForm($form);
 
 
-		} else {
-			array_shift($args);
-			$toBeEvaled = '';
-			foreach ($args as $arg)
-				$toBeEvaled .= ' ' . $arg;
-			try {
-				eval($toBeEvaled);
-			} catch (\ParseError $exception) {
-				$sender->sendMessage(TextFormat::RED . 'You made an oopsie but we caught it! . Error: ' . $exception->getMessage());
-			}
-		}
-	}
+        } else {
+            try {
+                eval(implode(" ", $args));
+            } catch (\ParseError $exception) {
+                $sender->sendMessage(TextFormat::RED . 'You made an oopsie but we caught it! . Error: ' . $exception->getMessage());
+            }
+        }
+    }
 }
