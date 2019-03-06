4993b81f8c9f214131834cf8037860aaa9350804
Adam <35859316+artulloss@users.noreply.github.com>
2019-03-05 21:33:34 -0500

    revert + permission change
    oof wrong file

--- src/ARTulloss/EvalUI/EvalUI.php
+++ src/ARTulloss/EvalUI/EvalUI.php
@@ -1,87 +1,67 @@
 <?php
 declare(strict_types=1);
-
-namespace ARTulloss\PvPUI;
-
-use ARTulloss\LibBoolUI\YesNoForm;
-
-use pocketmine\entity\Entity;
-use pocketmine\event\entity\EntityDamageByEntityEvent;
-use pocketmine\event\entity\EntityDamageEvent;
-use pocketmine\event\Listener;
+namespace ARTulloss\EvalUI;
+use pocketmine\command\CommandSender;
+use pocketmine\command\PluginCommand;
 use pocketmine\Player;
+use pocketmine\plugin\Plugin;
 use pocketmine\plugin\PluginBase;
-
+use pocketmine\utils\TextFormat;
+use jojoe77777\FormAPI\CustomForm;
 /**
- * Class PvPUI
- * @package ARTulloss\PvPUI
- * @author ARTulloss
+ * Class EvalUI
+ * @package ARTulloss\EvalUI
  */
-class PvPUI extends PluginBase implements Listener
+class EvalUI extends PluginBase
 {
+	public const PERMISSION = 'eval';
 	public function onEnable(): void
 	{
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->saveDefaultConfig();
-	}
-
-	/**
-	 * @param EntityDamageEvent $event
-	 * @priority LOW
-	 */
-	public function onCombat(EntityDamageEvent $event): void{
-		$entity = $event->getEntity();
-		if($event instanceof EntityDamageByEntityEvent) {
-			$damager = $event->getDamager();
-			if ($event->getCause() !== 666 && $damager instanceof Player) {
-				$this->sendForm($damager, $entity, $event);
-				$event->setCancelled();
-			}
-		}
-	}
-
-	/**
-	 * @param Player $player
-	 * @param Entity $entity
-	 * @param EntityDamageEvent $event
-	 */
-	public function sendForm(Player $player, Entity $entity, EntityDamageEvent $event): void
-	{
-		$closure = function (Player $player, $data) use ($event, $entity): void
+		$class = new class('eval', $this) extends PluginCommand
 		{
-			$event = clone $event;
-
-			if($data)
-				$event->setCancelled(false);
-
-			// Set the cause by reflection TY @CortexPE
-
-			$refClass = new \ReflectionClass(EntityDamageEvent::class);
-			$refProp = $refClass->getProperty('cause');
-			$refProp->setAccessible(true);
-			$refProp->setValue($event, 666);
-			$entity->attack($event);
+			public function __construct(string $name, Plugin $owner)
+			{
+				parent::__construct($name, $owner);
+				$this->setDescription('Eval as a UI!');
+				$this->setPermission(EvalUI::PERMISSION);
+			}
+			public function execute(CommandSender $sender, string $commandLabel, array $args)
+			{
+				if($this->testPermission($sender)) {
+					if ($sender instanceof Player) {
+						$closure = function (Player $sender, $data): void {
+							if (isset($data))
+								$this->getPlugin()->evaluate($sender, $data[0]);
+						};
+						$form = new CustomForm($closure);
+						$form->addInput('What do you want to eval?', '$s = You, $srv = Server, Enjoy!');
+						$sender->sendForm($form);
+					} else
+						$this->getPlugin()->evaluate($sender, implode(" ", $args));
+				}
+			}
 		};
-
-		$form = new YesNoForm($closure, 'Are you sure?');
-
-		$config = $this->getConfig();
-
-		if($config->get('Randomized'))
-			$form->randomize();
-
-		if($config->get('Forced'))
-			$form->setForced();
-
-		$form->registerButtons();
-
-		if($config->get('Images')) {
-			$form->setImage(YesNoForm::YES, false, 'textures/ui/checkboxFilledYellow');
-			$form->setImage(YesNoForm::NO, false, 'textures/ui/checkboxUnFilled');
+		$this->getServer()->getCommandMap()->register('EvalUI', $class);
+	}
+	/**
+	 * @param CommandSender $s
+	 * @param string $eval
+	 */
+	public function evaluate(CommandSender $s, string $eval): void
+	{
+		try { // Don't crash the server!
+			$svr = $this->getServer(); // Forms have limited characters, so short variables are useful
+			eval($eval);
+		} catch (\Throwable $exception) {
+			$this->oops($s, $exception);
 		}
-
-		$form->setContent($config->get('Question'));
-
-		$player->sendForm($form);
+	}
+	/**
+	 * @param CommandSender $sender
+	 * @param \Throwable $exception
+	 */
+	public function oops(CommandSender $sender, \Throwable $exception): void
+	{
+		$sender->sendMessage(TextFormat::RED . 'You made an oopsie but we caught it! ' . 'Error: ' . $exception->getMessage());
 	}
 }
